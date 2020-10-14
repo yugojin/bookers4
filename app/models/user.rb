79a7180b1246f946803d282a43f2a5eb4637e8e8
EC2 Default User <ec2-user@ip-172-31-53-127.ec2.internal>
2020-10-14 09:12:19 +0000

    [Update]user画面のbook投稿手前まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,4 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
 end
