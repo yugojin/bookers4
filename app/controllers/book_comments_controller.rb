285168677d37d0df8fe707939d679a66d28675fe
EC2 Default User <ec2-user@ip-172-31-13-9.ap-northeast-1.compute.internal>
2020-11-02 11:48:31 +0000

    [Update]応用課題３コメント機能View画面手前まで

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,8 @@
+class BookCommentsController < ApplicationController
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,6 @@
 class Book < ApplicationRecord
   belongs_to :user
+  has_many :book_comment, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
   def favorited_by?(user)
